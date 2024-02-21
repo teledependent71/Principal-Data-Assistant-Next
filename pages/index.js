ceb73653e171f3572780a9f911615f8fc2d8c66a
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2024-02-22 00:39:38 +0200

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Principal Data Assistant</title>
+          <meta property="og:title" content="Principal Data Assistant" />
+        </Head>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
---
+++ pages/test-page.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/react-components'
+import { getEntityByAttribute } from '@teleporthq/cms-mappers/caisy'
+
+const TestPage = (props) => {
+  return (
+    <>
+      <div className="test-page-container">
+        <Head>
+          <title>test-page - Principal Data Assistant</title>
+          <meta
+            property="og:title"
+            content="test-page - Principal Data Assistant"
+          />
+        </Head>
+        <DataProvider
+          renderSuccess={(context_l2ch1s) => (
+            <>
+              <h1 id={context_l2ch1s?.id}>Heading</h1>
+            </>
+          )}
+          initialData={props.contextL2ch1sProp}
+          persistDataDuringLoading={true}
+          key={props?.contextL2ch1sProp?.id}
+        />
+      </div>
+      <style jsx>
+        {`
+          .test-page-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default TestPage
+
+export async function getStaticProps(context) {
+  try {
+    const contextL2ch1sProp = await getEntityByAttribute({
+      ...context?.params,
+      projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+      query:
+        'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+      attribute: 'id',
+      id: '2',
+    })
+    return {
+      props: {
+        contextL2ch1sProp: contextL2ch1sProp?.data?.[0],
+      },
+      revalidate: 60,
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
+  }
+}
